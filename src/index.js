c57a38f18fd68ba61dd491a5b7eebc1b11fb77bf
zxd6654 <739971915@qq.com>
2019-09-20 14:52:55 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/style.css'; // 引入css
+
+console.log('这里是打包文件入口-index.js');
+
+class BabelUse {
+
+}
+
+export {
+    BabelUse
+}
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+/*
+ * @Author: zxd 
+ * @Date: 2019-09-17 15:30:36 
+ * @Last Modified by: zxd
+ * @Last Modified time: 2019-09-20 14:00:17
+ */
+const path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+let {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+
+    output: {
+        filename: 'bundle[hash:4].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: 'babel-loader',
+                include: /src/,
+                exclude: /node_modules/
+            },
+            //引用css
+            // {
+            //     test: /\.css$/,
+            //     use: ['style-loader', 'css-loader']
+            // },
+            {
+                test: /\.css$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    use: ['css-loader', 'postcss-loader'],
+                    publicPath: '../'
+                })
+            },
+            {
+                test: /\.jpg$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        outputPath: 'img/',
+                    }
+                }]
+            },
+            {
+                test: /\.(html|htm)$/,
+                use: 'html-withimg-loader',
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            hash: true,
+        }),
+        new ExtractTextWebpackPlugin('css/style.css'),
+
+        new CleanWebpackPlugin(),
+    ],
+
+    devServer: {
+        contentBase: './dist',
+        host: 'localhost',
+        port: '8888',
+        open: true,
+        hot: true,
+    },
+
+    mode: 'development', //development or production
+}
