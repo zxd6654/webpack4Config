7bb12bed25590eeb40c3c8b5a2008e3e45bc62c7
zxd6654 <739971915@qq.com>
2019-10-25 11:24:15 +0800

    webpack打包通过library和

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: zxd 
  * @Date: 2019-09-17 15:30:36 
  * @Last Modified by: zxd
- * @Last Modified time: 2019-09-20 14:53:51
+ * @Last Modified time: 2019-10-25 11:12:12
  */
 const path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -19,6 +19,12 @@ module.exports = {
     output: {
         filename: 'bundle[hash:4].js',
         path: path.resolve(__dirname, 'dist'),
+        //在script标签里或者浏览器console里调用webpack打包后的函数
+        library: '', //导出库的名字   
+        libraryTarget: 'umd',
+        //导出方式 由于浏览器环境和node环境的区别，所以产生了window（客服端浏览器）和global（node服务端）的区别。
+        //我理解的var即在script导入时和window一致， 是否可以通过import导入， 导入之后的使用还待解释。
+        //umd即支持所有情况的自定义。
     },
 
     module: {
